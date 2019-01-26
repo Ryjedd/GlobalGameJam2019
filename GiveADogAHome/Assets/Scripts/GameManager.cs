e2352e84effc277875c76ea58f008ce6f30de273
Ryjedd <rlambert92@live.ca>
2019-01-26 16:04:30 -0500

    dog spawn fixes

--- GiveADogAHome/Assets/Scripts/GameManager.cs
+++ GiveADogAHome/Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public float timeLeft = 30.0f;
+    public bool isGameOver = false;
+    public Image gameOverScreen;
+
     List<GameObject> dogList = new List<GameObject>();
     public int dogNum = 10;
     private const int MAX_DOGS = 50;
@@ -20,34 +25,43 @@ public class GameManager : MonoBehaviour
 
     private GameObject player1;
     private GameObject player2;
-    private float speed = 6.0f;
+    private float speed = 10.0f;
+
+    public Text player1_goldScore;
+    public Text player1_silverScore;
+    public Text player1_bronzeScore;
+    public Text player1_dogScore;
+    public Text player2_goldScore;
+    public Text player2_silverScore;
+    public Text player2_bronzeScore;
+    public Text player2_dogScore;
+    public Text Timer;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        //GameObject dog = (GameObject)Instantiate(Resources.Load("Prefabs/Dog")); 
-        //dog.GetComponent<Dog>().setName("kiwidog");
-        //Debug.Log(dog.GetComponent<Dog>().getName());
-        
-
-        InvokeRepeating("InstantiateDogs", 0.0f, 10.0f);
-        InvokeRepeating("InstantiateTreats", 0.0f, 5.0f);
-
+        //Setting Player 1
         player1 = Instantiate(Resources.Load("Prefabs/Player"), new Vector3(0 - 30.0F, 1, 25), Quaternion.identity) as GameObject;
         player1.GetComponent<Player>().setCharacter(player1);
         player1.transform.localScale = Vector3.one;
-        player1.GetComponent<Player>().setName("kiwi");
+
+        //Setting Player 2
+        player2 = Instantiate(Resources.Load("Prefabs/Player"), new Vector3(0 - 30.0F, 1, 25), Quaternion.identity) as GameObject;
+        player2.GetComponent<Player>().setCharacter(player2);
+        player2.transform.localScale = Vector3.one;
+
+        InvokeRepeating("InstantiateDogs", 0.0f, 10.0f);
+        InvokeRepeating("InstantiateTreats", 0.0f, 5.0f);
     }
 
 
     void InstantiateDogs()
     {
         //instantiate dogs
-        int newDogNum = Random.Range(5, 15);
+        int newDogNum = Random.Range(1, 15);
         if (newDogNum + dogNum <= MAX_DOGS)
         {
-            for (int i = 0; i < dogNum; i++)
+            for (int i = 0; i < newDogNum; i++)
             {
                 int dogType = Random.Range(1, 4);
                 Debug.Log(dogType);
@@ -73,7 +87,7 @@ public class GameManager : MonoBehaviour
         } else if(dogNum < MAX_DOGS)
         {
             newDogNum = MAX_DOGS - dogNum;
-            for (int i = 0; i < dogNum; i++)
+            for (int i = 0; i < newDogNum; i++)
             {
                 int dogType = Random.Range(1, 4);
                 Debug.Log(dogType);
@@ -162,16 +176,64 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void displayPlayer1Text()
+    {
+        player1_goldScore.text = "GoldTreat: " + player1.GetComponent<Player>().getGoldTreatCount().ToString();
+        player1_silverScore.text = "silverTreat: " + player1.GetComponent<Player>().getSilverTreatCount().ToString();
+        player1_bronzeScore.text = "bronzeTreat: " + player1.GetComponent<Player>().getBronzeTreatCount().ToString();
+        player1_dogScore.text = "Dog Score: " + player1.GetComponent<Player>().getDogCount().ToString();
+    }
+
+    void displayPlayer2Text()
+    {
+        player2_goldScore.text = "GoldTreat: " + player2.GetComponent<Player>().getGoldTreatCount().ToString();
+        player2_silverScore.text = "silverTreat: " + player2.GetComponent<Player>().getSilverTreatCount().ToString();
+        player2_bronzeScore.text = "bronzeTreat: " + player2.GetComponent<Player>().getBronzeTreatCount().ToString();
+        player2_dogScore.text = "Dog Score: " + player2.GetComponent<Player>().getDogCount().ToString();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        if (!isGameOver)
         {
-            float mH = Input.GetAxis("Horizontal");
-            float mV = Input.GetAxis("Vertical");
-            player1.GetComponent<Rigidbody>().velocity = new Vector3(mH * speed, player1.GetComponent<Rigidbody>().velocity.y, mV * speed);
+            gameOverScreen.enabled = false;
+            timeLeft -= Time.deltaTime;
+            Timer.text = "Time Remaining: " + timeLeft;
+            Debug.Log(timeLeft);
+            //getting player1 movement
+            float mH1 = Input.GetAxis("Horizontal");
+            float mV1 = Input.GetAxis("Vertical");
+            player1.GetComponent<Rigidbody>().velocity = new Vector3(mH1 * speed, player1.GetComponent<Rigidbody>().velocity.y, mV1 * speed);
+
+            //getting player2 movement
+            float mH2 = Input.GetAxis("Horizontal_P2");
+            float mV2 = Input.GetAxis("Vertical_P2");
+            player2.GetComponent<Rigidbody>().velocity = new Vector3(mH2 * speed, player2.GetComponent<Rigidbody>().velocity.y, mV2 * speed);
+
+            displayPlayer1Text();
+            displayPlayer2Text();
+            if (timeLeft < 0)
+            {
+                gameOver();
+            }
         }
-        //Debug.Log(player1.GetComponent<Player>().getTreatCount());
+
+        void gameOver()
+        {
+            isGameOver = true;
+            int player1Score = player1.GetComponent<Player>().getDogCount();
+            int player2Score = player2.GetComponent<Player>().getDogCount();
+
+            gameOverScreen.enabled = true;
+            if (player1Score > player2Score)
+            {
+                Debug.Log("Player 1 wins!!");
+            }
+            else
+            {
+                Debug.Log("Player 2 wins!!");
+            }
+        }
     }
 }
