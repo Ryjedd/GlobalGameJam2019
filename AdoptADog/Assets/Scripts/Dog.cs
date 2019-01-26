05345ddfc2952ad5ff996d3df1db26f395411ea5
Ryjedd <rlambert92@live.ca>
2019-01-25 21:50:00 -0500

    Rename scene just in case

---
+++ AdoptADog/Assets/Scripts/Dog.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{	
+	protected string type;
+	protected bool ownerTreats;
+	protected new string name;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public string getName(){
+		return this.name;
+	}
+
+	public string getType(){
+		return this.type;
+	}
+
+	public bool getOwnerTreats(){
+		return this.ownerTreats;
+	}
+
+	public void setName(string name){
+		this.name = name;
+	}
+		
+}
---
+++ AdoptADog/Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+		GameObject dog = (GameObject)Instantiate(Resources.Load("Prefabs/Dog")); 
+		dog.GetComponent<Dog>().setName("kiwidog");
+		Debug.Log(dog.GetComponent<Dog>().getName());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
