863850b529ff3a57caa05a3495215d059cca83b5
Ryjedd <rlambert92@live.ca>
2019-01-26 09:15:33 -0500

    More dogs being dropped off
    Interval dog instantiation as more dogs are dropped off.

--- GiveADogAHome/Assets/Scripts/GameManager.cs
+++ GiveADogAHome/Assets/Scripts/GameManager.cs
@@ -4,28 +4,50 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject[] dogArray;
+    List<GameObject> dogList = new List<GameObject>();
     public int dogNum = 10;
+    private const int MAX_DOGS = 50;
     public GameObject dog; //selected in the editor
 
     // Start is called before the first frame update
     void Start()
     {
-		
-		//GameObject dog = (GameObject)Instantiate(Resources.Load("Prefabs/Dog")); 
-		//dog.GetComponent<Dog>().setName("kiwidog");
-		//Debug.Log(dog.GetComponent<Dog>().getName());
 
-        //instantiate dogs
-        dogArray = new GameObject[dogNum];
-        for (int i = 0; i < dogNum; i++)
-        {
-            GameObject go = Instantiate(dog, new Vector3((float)i, 1, 0), Quaternion.identity) as GameObject;
-            go.transform.localScale = Vector3.one;
-            dogArray[i] = go;
-        }
+        //GameObject dog = (GameObject)Instantiate(Resources.Load("Prefabs/Dog")); 
+        //dog.GetComponent<Dog>().setName("kiwidog");
+        //Debug.Log(dog.GetComponent<Dog>().getName());
+
+
+        InvokeRepeating("InstantiateDogs", 0.0f, 10.0f);
     }
 
+
+    void InstantiateDogs()
+    {
+        //instantiate dogs
+        int newDogNum = Random.Range(5, 15);
+        if (newDogNum + dogNum <= MAX_DOGS)
+        {
+            for (int i = 0; i < dogNum; i++)
+            {
+                GameObject go = Instantiate(dog, new Vector3((float)i - 30.0F, 1, 25), Quaternion.identity) as GameObject;
+                go.transform.localScale = Vector3.one;
+                dogList.Add(go);
+            }
+            dogNum += newDogNum;
+        } else if(dogNum < MAX_DOGS)
+        {
+            newDogNum = MAX_DOGS - dogNum;
+            for (int i = 0; i < dogNum; i++)
+            {
+                GameObject go = Instantiate(dog, new Vector3((float)i - 30.0F, 1, 25), Quaternion.identity) as GameObject;
+                go.transform.localScale = Vector3.one;
+                dogList.Add(go);
+            }
+            dogNum = MAX_DOGS;
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
